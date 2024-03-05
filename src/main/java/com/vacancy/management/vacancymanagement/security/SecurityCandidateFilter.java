0a59d613dd13bfff4f9d1642033c823d9f411ded
FredNeves95 <fredbneves95@gmail.com>
2024-03-05 17:53:10 -0300

    feat: new jwt provider and filters to candidate

--- src/main/java/com/vacancy/management/vacancymanagement/security/SecurityCandidateFilter.java
+++ src/main/java/com/vacancy/management/vacancymanagement/security/SecurityCandidateFilter.java
@@ -5,11 +5,12 @@ import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.vacancy.management.vacancymanagement.providers.JWTProvider;
+import com.vacancy.management.vacancymanagement.providers.JWTCandidateProvider;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -17,33 +18,38 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCandidateFilter extends OncePerRequestFilter {
 
   @Autowired
-  JWTProvider jwtProvider;
+  JWTCandidateProvider jwtCandidateProvider;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-        if(request.getRequestURI().startsWith("/company")){
+        if(request.getRequestURI().startsWith("/candidate")){
           SecurityContextHolder.getContext().setAuthentication(null);
           String header = request.getHeader("Authorization");
           
           if(header != null){
-            var subjectToken = this.jwtProvider.validateToken(header);
-  
+            var tokenDecoded = this.jwtCandidateProvider.validateToken(header);
+            var subjectToken = tokenDecoded.getSubject();
+            var roles = tokenDecoded.getClaim("roles").asList(Object.class);
+            var grants = roles.stream()
+              .map(
+                role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())
+              ).toList();
+            
             if(subjectToken.isEmpty()){
               response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
               return;
             }
-  
-            request.setAttribute("company_id", subjectToken);
-           
-            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
-  
+
+            request.setAttribute("candidate_id", subjectToken);          
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, grants);
+
             SecurityContextHolder.getContext().setAuthentication(auth);
-          }
-        }
+        }}
 
         filterChain.doFilter(request, response);
   }
